1d7972a3661ed85298119e202006dd6075466fa2
stephen miracle <miracle@stephens-MBP.attlocal.net>
2020-07-31 15:27:09 -0400

    finalizing verification.

--- main.js
+++ main.js
@@ -2,6 +2,55 @@ const jwksRsa = require('jwks-rsa');
 const jsonwebtoken = require("jsonwebtoken");
 
 
-module.exports = () => {
-    jsonwebtoken
+/**
+ * 
+ * @param {string} token 
+ * @param {string} domain 
+ * @param {boolean} shouldCache 
+ * @param {number} cacheMaxAge 
+ * @param {boolean} shouldRateLimit 
+ * @param {boolean} requestsPerMinute 
+ * @param {boolean} shouldStrictSsl 
+ */
+exports.verify = (
+    token, 
+    domain, 
+    shouldCache = true, 
+    cacheMaxAge = 86400000, 
+    shouldRateLimit = true, 
+    requestsPerMinute = 10,
+    shouldStrictSsl = true) => {
+
+        console.log(token);
+    const keyClient = jwksRsa({
+        cache: shouldCache,
+        cacheMaxAge: shouldCache,
+        rateLimit: shouldRateLimit,
+        jwksRequestsPerMinute: requestsPerMinute,
+        strictSsl: shouldStrictSsl,
+        jwksUri: `${domain}/.well-known/jwks.json`
+    });
+    
+
+    const getSigningKey = (header = decoded.header, callback) => {
+        keyClient.getSigningKey(header.kid, function(err, key) {
+            const signingKey = key.publicKey || key.rsaPublicKey;
+            callback(null, signingKey);
+        })
+    }
+
+    return new Promise((resolve, reject) => {
+        jsonwebtoken.verify(
+            token, 
+            getSigningKey,
+            { "algorithms": "RS256"},
+            (error, decoded) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(decoded);
+                }
+            }
+        );
+    })
 }
